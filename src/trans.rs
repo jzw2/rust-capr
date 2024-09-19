9103f07ec732ab84d1037bbf2f664c6cf712e0d6
John Wang <johnz.wang@outlook.com>
2024-09-19 17:28:41 -0500

    Add more negate tests

--- src/trans.rs
+++ src/trans.rs
@@ -85,6 +85,10 @@ fn negate(fst: &SoundFst, alphabet: &[Label]) -> SoundFst {
     }
     ret.set_final(accept, ProbabilityWeight::one());
 
+    if ret.start().is_none() {
+        ret.set_start(accept);
+    }
+
     ret
 }
 
@@ -372,5 +376,103 @@ mod tests {
 
         dbg!(&negate_fst);
         assert!(accepts(&negate_fst, &input3));
+    }
+    
+        #[test]
+    fn negate_test_simple_string() {
+        // FST that accepts [1,2,3]
+        let mut fst: SoundFst = fst![1, 2, 3];
+        let alpha = vec![1, 2, 3, 4, 5, 6];
+
+        let mut det_fst = determinize(&fst).unwrap();
+        rm_epsilon(&mut det_fst).unwrap();
+
+        let negate_fst = negate(&det_fst, &alpha);
+
+        let input = vec![1, 2, 3];
+        assert!(accepts(&det_fst, &input));
+        assert!(!accepts(&negate_fst, &input));
+
+        let other_input = vec![4, 5, 6];
+        assert!(!accepts(&det_fst, &other_input));
+        assert!(accepts(&negate_fst, &other_input));
+    }
+    #[test]
+    fn negate_test_empty_string() {
+        // FST that accepts the empty string
+        let mut fst: SoundFst = SoundFst::new();
+        let s0 = fst.add_state();
+        fst.set_start(s0).unwrap();
+        fst.set_final(s0, ProbabilityWeight::one()).unwrap();
+
+        let alpha = vec![1];
+
+        let mut det_fst = determinize(&fst).unwrap();
+        rm_epsilon(&mut det_fst).unwrap();
+
+        let negate_fst = negate(&det_fst, &alpha);
+
+        let empty_input: Vec<Label> = vec![];
+        let non_empty_input = vec![1];
+
+        assert!(accepts(&det_fst, &empty_input));
+        assert!(!accepts(&det_fst, &non_empty_input));
+
+        assert!(!accepts(&negate_fst, &empty_input));
+        assert!(accepts(&negate_fst, &non_empty_input));
+    }
+#[test]
+    fn negate_test_all_strings() {
+        // FST that accepts all strings over the alphabet
+        let mut fst: SoundFst = SoundFst::new();
+        let s0 = fst.add_state();
+        fst.set_start(s0).unwrap();
+        fst.set_final(s0, ProbabilityWeight::one()).unwrap();
+        let alpha = vec![1, 2, 3];
+
+        for &label in &alpha {
+            fst.add_tr(s0, Tr::new(label, label, ProbabilityWeight::one(), s0))
+                .unwrap();
+        }
+
+        let mut det_fst = determinize(&fst).unwrap();
+        rm_epsilon(&mut det_fst).unwrap();
+
+        let negate_fst = negate(&det_fst, &alpha);
+
+        let any_input = vec![1, 2, 3];
+        let empty_input: Vec<Label> = vec![];
+
+        assert!(accepts(&det_fst, &any_input));
+        assert!(accepts(&det_fst, &empty_input));
+
+        assert!(!accepts(&negate_fst, &any_input));
+        assert!(!accepts(&negate_fst, &empty_input));
+    }
+  #[test]
+    fn negate_test_no_strings() {
+        // FST that accepts no strings
+        let mut fst: SoundFst = SoundFst::new();
+        let s0 = fst.add_state();
+        fst.set_start(s0).unwrap();
+        // No final states are set
+        let alpha = vec![1, 2, 3];
+
+        let mut det_fst = determinize(&fst).unwrap();
+        rm_epsilon(&mut det_fst).unwrap();
+
+        let negate_fst = negate(&det_fst, &alpha);
+        dbg!(&negate_fst);
+        dbg!(&det_fst);
+        negate_fst.draw("negate_test.txt", &DrawingConfig::default());
+
+        let any_input = vec![1, 2, 3];
+        let empty_input: Vec<Label> = vec![];
+
+        assert!(!accepts(&det_fst, &any_input));
+        assert!(!accepts(&det_fst, &empty_input));
+
+        assert!(accepts(&negate_fst, &any_input));
+        assert!(accepts(&negate_fst, &empty_input));
     }
 }
