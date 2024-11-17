c43790260670ab22422e5073ce700a4f39494137
John Wang <johnz.wang@outlook.com>
2024-11-17 18:08:46 +0100

    Add create fst button

--- index.ts
+++ index.ts
@@ -1,44 +1,43 @@
 import init, {
+  create_law,
+  SoundLaw,
   transduce_context,
   transduce_context_invert,
-  greet,
 } from "./pkg/rust_capr";
+
+var currentInput: SoundLaw | null = null;
+
+function setLaw() {
+  const left = (document.getElementById("left") as HTMLInputElement).value;
+  const right = (document.getElementById("right") as HTMLInputElement).value;
+  const to = (document.getElementById("to") as HTMLInputElement).value;
+  const from = (document.getElementById("from") as HTMLInputElement).value;
+  currentInput = create_law(left, right, from, to);
+}
 
 function transduce() {
   (document.getElementById("output") as HTMLParagraphElement).innerText =
     "Loading...";
 
-  const left = (document.getElementById("left") as HTMLInputElement).value;
-  const right = (document.getElementById("right") as HTMLInputElement).value;
-  const to = (document.getElementById("to") as HTMLInputElement).value;
-  const from = (document.getElementById("from") as HTMLInputElement).value;
   const input = (document.getElementById("input") as HTMLInputElement).value;
   const backward = (document.getElementById("backward") as HTMLInputElement)
     .value;
 
-  let result = transduce_context(left, right, from, to, input);
-  let backward_result = transduce_context_invert(
-    left,
-    right,
-    from,
-    to,
-    backward,
-  );
-
-  console.log(`${result}, ${result.length}`);
-  console.log(`${backward_result}, ${backward.length}`);
-  (document.getElementById("output") as HTMLParagraphElement).innerText = result
-    .join("")
-    .split(" ")
-    .filter((s) => s != " ")
-    .join("");
-  (
-    document.getElementById("backwards-output") as HTMLParagraphElement
-  ).innerText =
-    "num outputs outputs" +
-    backward_result.length +
-    "\n" +
-    backward_result.join("\n");
+  if (currentInput != null) {
+    let result = currentInput.transduce_text(input);
+    let backward_result = currentInput.transduce_text_backwards(backward);
+    console.log(`${result}, ${result.length}`);
+    console.log(`${backward_result}, ${backward.length}`);
+    (document.getElementById("output") as HTMLParagraphElement).innerText =
+      result
+        .join("")
+        .split(" ")
+        .filter((s) => s != " ")
+        .join("");
+    (
+      document.getElementById("backwards-output") as HTMLParagraphElement
+    ).innerText = "\n" + backward_result.join("\n");
+  }
 }
 
 async function run() {
@@ -47,7 +46,7 @@ async function run() {
   console.log("Event listeners!");
 
   // List of all input field ids that need an event listener
-  const inputIds = ["left", "right", "from", "to", "input", "backward"];
+  const inputIds = ["input", "backward"];
 
   inputIds.forEach((id) => {
     (document.getElementById(id) as HTMLInputElement).addEventListener(
@@ -55,6 +54,11 @@ async function run() {
       () => transduce(),
     );
   });
+
+  (document.getElementById("create-law") as HTMLButtonElement).addEventListener(
+    "onClick",
+    () => setLaw(),
+  );
 }
 
 run();
