187ad7a1893a3ab159d0ffed942e1b843d907874
John Wang <johnz.wang@outlook.com>
2024-11-20 15:20:18 +0100

    Add composition to front end

--- index.ts
+++ index.ts
@@ -3,45 +3,74 @@ import init, {
   SoundLaw,
   transduce_context,
   transduce_context_invert,
+  SoundLawComposition,
 } from "./pkg/rust_capr";
 
 var currentInput: SoundLaw | null = null;
 
 function setLaw() {
+  (document.getElementById("output") as HTMLParagraphElement).innerText =
+    "Compiling FST";
+  (
+    document.getElementById("backwards-output") as HTMLParagraphElement
+  ).innerText = "Compiling FST";
   const left = (document.getElementById("left") as HTMLInputElement).value;
   const right = (document.getElementById("right") as HTMLInputElement).value;
   const to = (document.getElementById("to") as HTMLInputElement).value;
   const from = (document.getElementById("from") as HTMLInputElement).value;
-  currentInput = create_law(left, right, from, to);
+  let currentInput = create_law(left, right, from, to);
+  currentLaws.push(currentInput);
+  fst?.add_law(currentInput);
+  transduce();
+}
+
+let currentLaws: SoundLaw[] = [];
+let fst: SoundLawComposition | null = null;
+
+function updateRulesList() {
+  const rulesList = document.getElementById(
+    "rulesList",
+  ) as HTMLParagraphElement;
+  rulesList.innerHTML = "";
+  currentLaws.forEach((x) => {
+    let s = `Rule: ${x.get_from()} → ${x.get_to()} / ${x.get_left_context()} _ ${x.get_right_context()}`;
+    const listItem = document.createElement("li");
+    listItem.textContent = s;
+    rulesList.appendChild(listItem);
+  });
 }
 
 function transduce() {
   (document.getElementById("output") as HTMLParagraphElement).innerText =
     "Loading...";
+  (
+    document.getElementById("backwards-output") as HTMLParagraphElement
+  ).innerText = "Loading...";
 
   const input = (document.getElementById("input") as HTMLInputElement).value;
   const backward = (document.getElementById("backward") as HTMLInputElement)
     .value;
 
-  if (currentInput != null) {
-    let result = currentInput.transduce_text(input);
-    let backward_result = currentInput.transduce_text_backwards(backward);
+  if (fst != null) {
+    let result = fst.transduce_text(input);
+    // let backward_result = fst.transduce_text_backwards(backward);
     console.log(`${result}, ${result.length}`);
-    console.log(`${backward_result}, ${backward.length}`);
+    //console.log(`${backward_result}, ${backward.length}`);
     (document.getElementById("output") as HTMLParagraphElement).innerText =
       result
         .join("")
         .split(" ")
         .filter((s) => s != " ")
         .join("");
-    (
-      document.getElementById("backwards-output") as HTMLParagraphElement
-    ).innerText = "\n" + backward_result.join("\n");
+    // (
+    //   document.getElementById("backwards-output") as HTMLParagraphElement
+    // ).innerText = "\n" + backward_result.join("\n");
   }
 }
 
 async function run() {
   await init();
+  fst = SoundLawComposition.new();
 
   console.log("Event listeners!");
 
@@ -56,8 +85,11 @@ async function run() {
   });
 
   (document.getElementById("create-law") as HTMLButtonElement).addEventListener(
-    "onClick",
-    () => setLaw(),
+    "click",
+    () => {
+      setLaw();
+      updateRulesList();
+    },
   );
 }
 
