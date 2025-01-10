2a01a9708289cf3412c728dd4817959d0e57d18d
John Wang <johnz.wang@outlook.com>
2025-01-10 22:37:36 +0100

    AddSoundLaw message

---
+++ input.ts
@@ -0,0 +1,11 @@
+import init, {
+  create_law,
+  SoundLaw,
+  transduce_context,
+  transduce_context_invert,
+  SoundLawComposition,
+  soundlaw_xsampa_to_ipa,
+} from "./pkg/rust_capr";
+const input = (props: { fst: SoundLawComposition }) => {
+  const input = useState;
+};
