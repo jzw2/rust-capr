6c585cd857c3ffb3eca92f569c69ad329a564dd1
John Wang <johnz.wang@outlook.com>
2025-04-24 13:24:01 +0200

    Add wrappers for regexes

--- src/regex.rs
+++ src/regex.rs
@@ -1,4 +1,4 @@
-use rustfst::prelude::VectorFst;
+use rustfst::prelude::{concat::concat, union::union, VectorFst};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::js_sys::Symbol;
 
@@ -9,13 +9,15 @@ pub struct Regex(VectorFst<SoundWeight>);
 
 #[wasm_bindgen]
 impl Regex {
-    pub fn concat() {
-        unimplemented!();
+    pub fn concat(&mut self, other: &Regex) {
+        let _ = concat(&mut self.0, &other.0);
     }
-    pub fn disjoint() {
-        unimplemented!();
+    pub fn disjoint(&mut self, other: &Regex) {
+        // I don't know why I chose a different name but whatever
+        let _ = union(&mut self.0, &other.0);
     }
-    pub fn kleen() {
+    //implement the rest later
+    pub fn kleen(&mut self) {
         unimplemented!();
     }
 }
