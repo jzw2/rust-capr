ff685bda94c5b5cf7886d4707700b1230899b9ae
John Wang <johnz.wang@outlook.com>
2025-05-20 15:19:47 +0200

    Move FileArea to new file

---
+++ frontend/FileArea.ts
@@ -0,0 +1,83 @@
+import { SoundLawComposition, soundlaw_xsampa_to_ipa } from "../pkg/rust_capr";
+import { Main } from "./main";
+
+export class FileArea {
+  uploadFile: HTMLInputElement;
+  fileStrings: string[];
+  tranducer: SoundLawComposition;
+  table: HTMLTableRowElement;
+  tableHeader: HTMLTableRowElement;
+
+  parent: Main;
+  constructor(parent: Main, transducer: SoundLawComposition) {
+    this.tranducer = transducer;
+    this.parent = parent;
+    this.table = document.getElementById("file-inputs") as HTMLTableRowElement;
+    this.tableHeader = document.getElementById(
+      "file-headers",
+    ) as HTMLTableRowElement;
+
+    this.uploadFile = document.getElementById("upload") as HTMLInputElement;
+    this.fileStrings = [];
+
+    this.uploadFile.addEventListener("change", async () => {
+      if (this.uploadFile.files) {
+        let file = this.uploadFile.files[0];
+        const text = await file.text();
+
+        this.fileStrings = text.split("\n").filter((x) => x !== "");
+
+        this.parent.transduce();
+        this.renderHeader();
+      } else {
+        this.fileStrings = [];
+      }
+    });
+  }
+
+  renderHeader() {
+    let newChildren = this.fileStrings.map((text) => {
+      const elem = document.createElement("th");
+      elem.textContent = soundlaw_xsampa_to_ipa(text);
+      return elem;
+    });
+    this.tableHeader.replaceChildren(...newChildren);
+  }
+
+  transduce() {
+    let body = this.table.querySelector("tbody");
+    if (!body) {
+      body = document.createElement("tbody");
+      this.table.appendChild(body);
+    }
+
+    const transduced = this.fileStrings.map((s) =>
+      this.tranducer.transduce_text(s),
+    );
+
+    if (transduced[0]) {
+      const transpose = transduced[0].map((_, index) =>
+        transduced.map((row) => {
+          if (!row[index]) {
+            return "";
+          }
+          return soundlaw_xsampa_to_ipa(row[index].replaceAll(" ", ""));
+        }),
+      );
+
+      const newChildren = transpose.map((r) => {
+        const row = document.createElement("tr");
+        const rowElements = r.map((d) => {
+          const data = document.createElement("td");
+          data.innerText = soundlaw_xsampa_to_ipa(d);
+          return data;
+        });
+        row.append(...rowElements);
+        return row;
+      });
+      body.replaceChildren(...newChildren);
+    } else {
+      body.replaceChildren();
+    }
+  }
+}
