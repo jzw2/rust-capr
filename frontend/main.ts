21441b9cbea5f5253801dbc44dae6d0175316c5e
John Wang <johnz.wang@outlook.com>
2025-05-20 14:18:10 +0200

    Add transduction and rendering to FileArea

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,13 +1,26 @@
-import { SoundLaw, SoundLawComposition } from "../pkg/rust_capr";
+import {
+  SoundLaw,
+  soundlaw_xsampa_to_ipa,
+  SoundLawComposition,
+} from "../pkg/rust_capr";
 import { DragType, RegexType, SoundClass, SoundLawInput } from "./types";
 
 class FileArea {
   uploadFile: HTMLInputElement;
-  fileStrings: String[];
+  fileStrings: string[];
   tranducer: SoundLawComposition;
+  table: HTMLTableRowElement;
+  tableHeader: HTMLTableRowElement;
+
   parent: Main;
   constructor(parent: Main) {
+    this.table = document.getElementById("file-inputs") as HTMLTableRowElement;
+    let tableHeader = document.getElementById(
+      "file-headers",
+    ) as HTMLTableRowElement;
+
     this.uploadFile = document.getElementById("upload") as HTMLInputElement;
+    this.fileStrings = [];
 
     this.uploadFile.addEventListener("change", async () => {
       if (this.uploadFile.files) {
@@ -15,24 +28,60 @@ class FileArea {
         const text = await file.text();
 
         this.fileStrings = text.split("\n").filter((x) => x !== "");
+
         this.parent.transduce();
+        this.renderHeader();
       } else {
         this.fileStrings = [];
       }
     });
   }
 
-  transduce() {}
-}
+  renderHeader() {
+    let newChildren = this.fileStrings.map((text) => {
+      const elem = document.createElement("th");
+      elem.textContent = soundlaw_xsampa_to_ipa(text);
+      return elem;
+    });
+    this.tableHeader.replaceChildren(...newChildren);
+  }
 
-class TranductionArea {
-  laws: SoundLaw[];
+  transduce() {
+    let body = this.table.querySelector("tbody");
+    if (!body) {
+      body = document.createElement("tbody");
+      this.table.appendChild(body);
+    }
 
-  constructor() {}
-}
+    const transduced = this.fileStrings.map((s) =>
+      this.tranducer.transduce_text(s),
+    );
 
-class SoundClassCreation {
-  regexType: RegexType;
+    const transpose = transduced[0].map((_, index) =>
+      transduced.map((row) => {
+        if (!row[index]) {
+          return "";
+        }
+        return soundlaw_xsampa_to_ipa(row[index].replaceAll(" ", ""));
+      }),
+    );
+
+    transpose.forEach((row) => {
+      const htmlRow = document.createElement("tr");
+      row.forEach((col) => {
+        const item = document.createElement("td");
+        item.textContent = col;
+        htmlRow.appendChild(item);
+      });
+      body.append(htmlRow);
+    });
+    let newChildren = this.fileStrings.map((text) => {
+      const elem = document.createElement("tr");
+      elem.textContent = soundlaw_xsampa_to_ipa(text);
+      return elem;
+    });
+    body.replaceChildren(...newChildren);
+  }
 }
 
 class Main {
@@ -55,6 +104,6 @@ class Main {
   }
 
   transduce() {
-    //todo
+    this.fileArea.transduce();
   }
 }
