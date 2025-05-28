a2db989d9fa6c69f3d1f674d9d0d6417608c6408
John Wang <johnz.wang@outlook.com>
2025-05-28 12:40:03 +0200

    Add save button class

---
+++ frontend/SaveButton.ts
@@ -0,0 +1,49 @@
+import { SoundClass } from "./types";
+
+class SaveButton {
+  saveButton: HTMLButtonElement;
+  loadButton: HTMLButtonElement;
+
+  soundClasses: SoundClass[];
+
+  saveListen: () => void;
+  loadListen: () => void;
+  constructor(soundClasses: SoundClass[]) {
+    this.soundClasses = soundClasses;
+
+    this.saveButton = document.getElementById("save") as HTMLButtonElement;
+    this.loadButton = document.getElementById("load") as HTMLButtonElement;
+
+    this.saveButton.addEventListener("click", () => {
+      // ideally make this smarter
+      localStorage.setItem("classes", JSON.stringify(this.soundClasses));
+      // try to use rust for this loading side
+      // localStorage.setItem("laws", JSON.stringify(state.soundLawInputs));
+      console.log("Storing classes");
+    });
+    this.loadButton.addEventListener("click", () => {
+      //todo!!!!!
+      // it would be a lot better to handle this in the rust side of things
+      // to make sure that we don't have horrible loading times
+      //   const classesStorage = localStorage.getItem("classes");
+      //         if (classesStorage) {
+      //           state.soundClasses = JSON.parse(classesStorage);
+      //         }
+      //         const storage = localStorage.getItem("laws");
+      //         if (storage) {
+      //           state.composition = SoundLawComposition.new();
+      //           state.laws = [];
+      //           state.soundLawInputs = [];
+      //           const laws = JSON.parse(storage) as SoundLawInput[];
+      //           state.isLoading = true;
+      //           setTimeout(() =>
+      //             laws.forEach((law) => {
+      //               state.soundLawInputs.push(law);
+      //               updateLaw({ type: "AddSoundLaw", law: law }, state).then((msg) =>
+      //                 sendMessage(msg),
+      //               );
+      //             }),
+      //           );
+    });
+  }
+}
